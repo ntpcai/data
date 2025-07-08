e2d626d23f251e7ac5e4be412c28e8d653734d4a
johncliu <john@beowulf.io>
2025-07-08 08:09:09 +0800

    removed old files

--- db/old/mergeall.sh
+++
@@ -1,3 +0,0 @@
-
-#jq -s "." */*.json | jq 'reduce .[] as $item ({}; .[$item.set][$item.subset] += $item.data)' > data.json
-jq -s "." */*.json | jq 'reduce .[] as $item ({}; .[$item.set] += {($item.subset): $item.data})' > data.json
--- db/old/subsettext.sh
+++
@@ -1,3 +0,0 @@
-#jq -sr '.[].data[] | "\(.fields.side1) \(.fields.side2) \(.fields.side2b)"' */*.json | jq -nR '[inputs | split("")] | flatten | unique' | jq -r 'join("")' > text.txt
-jq -sr '.[] | "\(.set) \(.subset) \(.data[].fields.side1) \(.data[].fields.side2) \(.data[].fields.side2b)"' */*.json | jq -nR '[inputs | split("")] | flatten | unique' | jq -r 'join("")' > text.txt
-glyphhanger text.txt > unicode.txt
